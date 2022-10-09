7aa23cc030ffd20961fbac8fda7fd1ad8f6d3cd9
Konovalov Kirill <kirkonowalow@gmail.com>
2022-10-09 14:21:41 +0300

    attempt 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -20,32 +20,35 @@ object ChatService {
     private var counterCid: Int = 0
 
     fun createEmptyChat(chat: Chat): Int {
-        userChats.add(Chats(chat.copy(cId = counterCid), emptyArray()))
+        userChats.find { chats -> chat.cId == chats.first.cId } ?: userChats.add(
+            Chats(
+                chat.copy(cId = counterCid),
+                emptyArray()
+            )
+        )
         counterCid += 1
         return counterCid - 1
     }
 
-    fun createChat(message: Message, ownerIdChat: Int, titleChat: String): Int {
-        userChats.add(Chats(Chat(counterCid, ownerIdChat, titleChat), arrayOf(message.copy(mId = counterMid))))
+    fun createChat(message: Message, ownerIdChat: Int, cId: Int, titleChat: String): Int {
+        userChats.find { chats -> cId == chats.first.cId && ownerIdChat == chats.first.ownerId } ?: userChats.add(
+            Chats(
+                Chat(counterCid, ownerIdChat, titleChat),
+                arrayOf(message.copy(mId = counterMid))
+            )
+        )
         counterMid += 1
         counterCid += 1
         return counterCid - 1
     }
 
-    fun addMessage(message: Message, ownerIdChat: Int): Int {
-        var check = false
-        for (chats in userChats) {
-            if (chats.first.cId == ownerIdChat) {
-                chats.second += message.copy(mId = counterMid)
-                counterMid += 1
-                check = true
-                return counterMid - 1
-            }
-        }
-        if (check == false) {
-            throw ChatNotFoundException("Chat not found")
-        }
-        return 0
+    fun addMessage(message: Message, ownerIdChat: Int, cId: Int): Int {
+        val chat = userChats.find { chats -> cId == chats.first.cId && ownerIdChat == chats.first.ownerId }
+            ?: throw ChatNotFoundException("Chat not found")
+        chat.second += message.copy(mId = counterMid)
+        userChats.set(userChats.indexOfFirst { cId == it.first.cId && ownerIdChat == it.first.ownerId }, chat)
+        counterMid += 1
+        return counterMid - 1
     }
 
     fun getUnreadReceivedChatsCount(ownerCid: Int): Int {
@@ -53,22 +56,16 @@ object ChatService {
         return unreadChats.count { chats -> chats.first.ownerId == ownerCid }
     }
 
-    fun getChats(ownerCid: Int){
-        //val chats = userChats.filter { chats -> chats.first.cId == ownerCid }
-        userChats.filter {chats -> chats.second.lastOrNull() ?: arrayOf("Пусто")
-            chats.first.cId == ownerCid
-        }
-
+    fun getChats(ownerCid: Int) {
+              val chat1 = userChats.filter { chats -> chats.first.cId == ownerCid }
     }
-
-
 
 
 }
 
 
 fun main(args: Array<String>) {
-    val chat1 = Chat(1, 122, "Chating" )
+    val chat1 = Chat(1, 122, "Chating")
     val chat2 = Chat(2, 123, "Chating2")
     val chat3 = Chat(3, 122, "Chating3")
     val message1 = Message(1, 122, "Hi", true)
@@ -77,13 +74,12 @@ fun main(args: Array<String>) {
     ChatService.createEmptyChat(chat1)
     ChatService.createEmptyChat(chat2)
     ChatService.createEmptyChat(chat3)
-    ChatService.addMessage(message1, 1)
+
     val v = Chats<Chat, Array<Message>>(first = chat1, second = arrayOf(message1, message2))
     val b = Chats<Chat, Array<Message>>(first = chat2, second = arrayOf(message3))
     var lista = mutableListOf<Chats<Chat, Array<Message>>>()
     lista += v
     lista += b
-
 
 
 }
